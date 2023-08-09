dfda76265ba845a5b59589e91250fd76deb39dd6
Varun Pal Singh <singhvarun928@gmail.com>
2023-08-09 16:58:41 +0530

    completed exp1 till compass 2

--- experiment/simulation/js/exp1.js
+++ experiment/simulation/js/exp1.js
@@ -9,6 +9,10 @@
 // VARIABLES;
 
 const trans1Step1 = document.querySelector("#trans1-step1");
+const dropMenu1Step1 = document.querySelector("#dropMenu1-step1");
+const pencil1Step1 = document.querySelector("#pencil1-step1");
+
+// once dom loaded then trans1Step1 will get activated.
 
 addEventListener("DOMContentLoaded", () => trans1Step1.style.visibility = "visible");
 
@@ -19,7 +23,8 @@ trans1Step1.addEventListener("click",() => {
     document.querySelector("#dropMenu1-step1").style.visibility = "visible";
 });
 
-const dropMenu1Step1 = document.querySelector("#dropMenu1-step1");
+
+// dropmenu 1 changes
 
 dropMenu1Step1.addEventListener("change",(evt)=>{
     let selectedValue = dropMenu1Step1.value;
@@ -32,13 +37,14 @@ dropMenu1Step1.addEventListener("change",(evt)=>{
     }
 });
 
-const trans2Step1 = document.querySelector("#trans2-step1");
+// when first H Pencil was clicked 
 
-document.querySelector("#pencil1-step1").addEventListener("click", (evt) => {
+pencil1Step1.addEventListener("click", (evt) => {
     evt.target.style.visibility = "hidden";
     document.querySelector("#png1-step1").style.visibility = "hidden";
     document.querySelector("#gif1-step1").style.visibility = "visible";
     document.querySelector("#arr3-step1").style.visibility = "hidden";
+
     setTimeout((evt) => {
         document.querySelector("#gif1-step1").style.visibility = "hidden";
         document.querySelector("#png2-step1").style.visibility = "visible";
@@ -48,31 +54,88 @@ document.querySelector("#pencil1-step1").addEventListener("click", (evt) => {
         document.querySelector("#arr1-step1").style.visibility = "visible";
         document.querySelector("#arr2-step1").style.visibility = "visible";
         trans2Step1.style.visibility = "visible";
-    }, 8000)
+    }, 10000)
 });
 
+const trans2Step1 = document.querySelector("#trans2-step1");
 const dropMenu2Step1 = document.querySelector("#dropMenu2-step1");
+const pencil2Step1 = document.querySelector("#pencil2-step1");
 
 trans2Step1.addEventListener("click", ()=> {
+    trans2Step1.style.visibility = "hidden";
     document.querySelector("#arr2-step1").style.visibility = "hidden";
     document.querySelector("#arr1-step1").style.visibility = "hidden";
     dropMenu2Step1.style.visibility = "visible";
     dropMenu2Step1.value = "default";
 });
 
+
 dropMenu2Step1.addEventListener("change",() => {
     let selectedValue = dropMenu2Step1.value;
     if(selectedValue === "3hPencil"){
         dropMenu2Step1.style.visibility = "hidden";
-        document.querySelector("#pencil2-step1").style.visibility = "visible";
+        pencil2Step1.style.visibility = "visible";
         document.querySelector("#arr4-step1").style.visibility = "visible";
     }else{
         alert("Please select 3H Pencil");
     }
 });
 
-document.querySelector("#pencil2-step1").addEventListener("click", (evt) => {
+pencil2Step1.addEventListener("click", (evt) => {
     evt.target.style.visibility = "hidden";
-    document.querySelector("#gif2-step1").style.visibility = "visible";
     document.querySelector("#arr4-step1").style.visibility = "hidden";
+
+    document.querySelector("#gif2-step1").style.visibility = "visible";
+
+    setTimeout(() => {
+    document.querySelector("#png3-step1").style.visibility = "visible";
+    document.querySelector("#gif2-step1").style.visibility = "hidden";
+    document.querySelector("#instructions-step1").innerText = "Now, Select compass";
+
+    document.querySelector("#arr1-step1").style.visibility = "visible";
+    document.querySelector("#arr5-step1").style.visibility = "visible";
+    trans3Step1.style.visibility = "visible";
+    }, 10000);
 });
+
+
+const trans3Step1 = document.querySelector("#trans3-step1");
+const comp1Step1 = document.querySelector("#compass1-step1");
+
+trans3Step1.addEventListener("click", ()=> {
+    trans3Step1.style.visibility = "hidden";
+    document.querySelector("#arr1-step1").style.visibility = "hidden";
+    document.querySelector("#arr5-step1").style.visibility = "hidden";
+
+    document.querySelector("#arr6-step1").style.visibility = "visible";
+    comp1Step1.style.visibility = "visible";
+});
+
+comp1Step1.addEventListener("click", (evt)=>{
+    evt.target.style.visibility = "hidden";
+    document.querySelector("#arr6-step1").style.visibility = "hidden";
+
+    document.querySelector("#gif3-step1").style.visibility = "visible";
+
+    setTimeout(() => {
+        tran4Step1.style.visibility = "visible";
+        document.querySelector("#arr7-step1").style.visibility = "visible";
+    }, 6000);
+});
+
+const tran4Step1 = document.querySelector("#trans4-step1");
+
+tran4Step1.addEventListener("click", (evt)=> {
+    evt.target.style.visibility = "hidden";
+    document.querySelector("#arr7-step1").style.visibility = "hidden";
+    document.querySelector("#gif3-step1").style.visibility = "hidden";
+
+    document.querySelector("#gif4-step1").style.visibility = "visible";
+
+    setTimeout(()=>{
+        // document.querySelector("#arr8-step1").style.visibility = "visible";
+        // tran5Step1.style.visibility = "visible";
+    }, 6000);
+});
+
+// const tran5Step1 = document.querySelector("#trans5-step1");
