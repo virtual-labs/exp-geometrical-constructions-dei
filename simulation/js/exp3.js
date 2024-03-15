ee6e6ecbb02c96741576d9279925e48b866825d6
vleadadmin <admin@vlabs.ac.in>
2024-03-15 11:14:11 +0000

    https://virtual-labs.github.io/virtual-labs/exp-geometrical-constructions-dei click on the link to test your code.

---
+++ simulation/js/exp3.js
@@ -0,0 +1,429 @@
+// VARIABLES
+
+// DROPMENUS, PENCILS, COMPASS AND INSTRUCTIONS
+const assemblyBox = document.querySelector("#assembly");
+const statementBox = document.querySelector("#exp3-statement");
+const instruction1Step3 = document.querySelector("#instructions-step3");
+
+const comp1Step3 = document.querySelector("#compass1-step3");
+const comp2Step3 = document.querySelector("#compass2-step3");
+
+const protect1Step3 = document.querySelector("#protector1-step3");
+
+const pencil1Step3 = document.querySelector("#pencil1-step3");
+const pencil2Step3 = document.querySelector("#pencil2-step3");
+const pencil3Step3 = document.querySelector("#pencil3-step3");
+const pencil4Step3 = document.querySelector("#pencil4-step3");
+const pencil5Step3 = document.querySelector("#pencil5-step3");
+
+const dropMenu1Step3 = document.querySelector("#dropMenu1-step3");
+const dropMenu2Step3 = document.querySelector("#dropMenu2-step3");
+const dropMenu3Step3 = document.querySelector("#dropMenu3-step3");
+const dropMenu4Step3 = document.querySelector("#dropMenu4-step3");
+const dropMenu5Step3 = document.querySelector("#dropMenu5-step3");
+
+// Images
+const png1Step3 = document.querySelector("#png1-step3");
+const png2Step3 = document.querySelector("#png2-step3");
+const png3Step3 = document.querySelector("#png3-step3");
+const png4Step3 = document.querySelector("#png4-step3");
+const png5Step3 = document.querySelector("#png5-step3");
+const png6Step3 = document.querySelector("#png6-step3");
+const png7Step3 = document.querySelector("#png7-step3");
+const png8Step3 = document.querySelector("#png8-step3");
+const finalPng = document.querySelector("#finalPng-step3");
+
+// GIFs
+const gif1Step3 = document.querySelector("#gif1-step3");
+const gif2Step3 = document.querySelector("#gif2-step3");
+const gif3Step3 = document.querySelector("#gif3-step3");
+const gif4Step3 = document.querySelector("#gif4-step3");
+const gif5Step3 = document.querySelector("#gif5-step3");
+const gif6Step3 = document.querySelector("#gif6-step3");
+const gif7Step3 = document.querySelector("#gif7-step3");
+const gif8Step3 = document.querySelector("#gif8-step3");
+const gif9Step3 = document.querySelector("#gif9-step3");
+const gif10Step3 = document.querySelector("#gif10-step3");
+const gif11Step3 = document.querySelector("#gif11-step3");
+const gif12Step3 = document.querySelector("#gif12-step3");
+const gif13Step3 = document.querySelector("#gif13-step3");
+const gif14Step3 = document.querySelector("#gif14-step3");
+const gif15Step3 = document.querySelector("#gif15-step3");
+const gif16Step3 = document.querySelector("#gif16-step3");
+const gif17Step3 = document.querySelector("#gif17-step3");
+const gif18Step3 = document.querySelector("#gif18-step3");
+const gif19Step3 = document.querySelector("#gif19-step3");
+const gif20Step3 = document.querySelector("#gif20-step3");
+
+
+// Transboxes
+const trans1Step3 = document.querySelector("#trans1-step3");
+const trans2Step3 = document.querySelector("#trans2-step3");
+const trans3Step3 = document.querySelector("#trans3-step3");
+const trans4Step3 = document.querySelector("#trans4-step3");
+const trans5Step3 = document.querySelector("#trans5-step3");
+const trans6Step3 = document.querySelector("#trans6-step3");
+const trans7Step3 = document.querySelector("#trans7-step3");
+const trans8Step3 = document.querySelector("#trans8-step3");
+const trans9Step3 = document.querySelector("#trans9-step3");
+const trans10Step3 = document.querySelector("#trans10-step3");
+const trans11Step3 = document.querySelector("#trans11-step3");
+const trans12Step3 = document.querySelector("#trans12-step3");
+const trans13Step3 = document.querySelector("#trans13-step3");
+
+
+// Arrows
+const arr1Step3 = document.querySelector("#arr1-step3");
+const arr2Step3 = document.querySelector("#arr2-step3");
+const arr3Step3 = document.querySelector("#arr3-step3");
+const arr4Step3 = document.querySelector("#arr4-step3");
+const arr5Step3 = document.querySelector("#arr5-step3");
+const arr6Step3 = document.querySelector("#arr6-step3");
+
+
+// Next buttons
+const arrNext = document.querySelector(".arr-next");
+const nextToFinalPng = document.querySelector("#nextToFinalPng");
+const nextBtnExp3 = document.querySelector("#nextToExpListFromExp3");
+
+
+// once dom loaded then trans1Step3 will get activated.
+addEventListener("DOMContentLoaded", () =>{
+    arr1Step3.style.visibility = "visible";
+    png1Step3.style.visibility = "visible";
+    trans1Step3.style.visibility = "visible";
+});
+
+trans1Step3.addEventListener("click",(evt) => {
+    evt.target.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+    dropMenu1Step3.style.visibility = "visible";
+});
+
+dropMenu1Step3.addEventListener("change",(evt)=>{
+    let selectedValue = dropMenu1Step3.value;
+    if(selectedValue === "hPencil"){
+        evt.target.style.visibility = "hidden";
+        pencil1Step3.style.visibility = "visible";
+        arr2Step3.style.visibility = "visible";
+    }else{
+        alert("Please select H Pencil");
+    }
+});
+
+pencil1Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    png1Step3.style.visibility = "hidden";
+    arr2Step3.style.visibility = "hidden";
+
+    gif1Step3.style.visibility = "visible";
+
+    setTimeout((evt) => {
+        gif1Step3.style.visibility = "hidden";
+        png2Step3.style.visibility = "visible";
+
+        instruction1Step3.innerText = "Select 3H pencil and extend the line to P such that AB=BP.";
+
+        arr1Step3.style.visibility = "visible";
+        trans2Step3.style.visibility = "visible";
+    }, 10000)
+});
+
+trans2Step3.addEventListener("click",(evt) => {
+    evt.target.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+    dropMenu2Step3.style.visibility = "visible";
+});
+
+dropMenu2Step3.addEventListener("change",(evt)=>{
+    let selectedValue = dropMenu2Step3.value;
+    if(selectedValue === "3hPencil"){
+        evt.target.style.visibility = "hidden";
+        pencil2Step3.style.visibility = "visible";
+        arr2Step3.style.visibility = "visible";
+    }else{
+        alert("Please select 3H Pencil");
+    }
+});
+
+pencil2Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    png2Step3.style.visibility = "hidden";
+    arr2Step3.style.visibility = "hidden";
+
+    gif2Step3.style.visibility = "visible";
+
+    setTimeout((evt) => {
+        gif2Step3.style.visibility = "hidden";
+        png3Step3.style.visibility = "visible";
+
+        instruction1Step3.innerText = "Select the compass and with B as the centre and AP as the radius, construct a semicircle.";
+
+        arr3Step3.style.visibility = "visible";
+        trans3Step3.style.visibility = "visible";
+    }, 13000);
+});
+
+trans3Step3.addEventListener("click",(evt) => {
+    evt.target.style.visibility = "hidden";
+    arr3Step3.style.visibility = "hidden";
+    comp1Step3.style.visibility = "visible";
+    arr2Step3.style.visibility = "visible";
+});
+
+comp1Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr2Step3.style.visibility = "hidden";
+    png3Step3.style.visibility = "hidden";
+
+    gif3Step3.style.visibility = "visible";
+
+    setTimeout(()=>{
+        gif3Step3.style.visibility = "hidden";
+        png4Step3.style.visibility = "visible";
+
+        instruction1Step3.innerText = "Select the protractor, and mark the points to divide the semicircle into 6 equal parts.";
+
+        trans4Step3.style.visibility = "visible";
+        arr4Step3.style.visibility = "visible";
+    }, 8000);
+});
+
+trans4Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr4Step3.style.visibility = "hidden";
+
+    protect1Step3.style.visibility = "visible";
+    arr5Step3.style.visibility = "visible";
+});
+
+protect1Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr5Step3.style.visibility = "hidden";
+    png4Step3.style.visibility = "hidden";
+
+    gif4Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        gif4Step3.style.visibility = "hidden";
+        png5Step3.style.visibility = "visible";
+
+        instruction1Step3.innerText = "Now, select the H pencil and join the 2nd division on the semicircle with point B.";
+        trans5Step3.style.visibility = "visible";
+        arr1Step3.style.visibility = "visible";
+    }, 24000);
+});
+
+trans5Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+
+    dropMenu3Step3.style.visibility = "visible";
+});
+
+dropMenu3Step3.addEventListener("change",(evt)=>{
+    let selectedValue = dropMenu3Step3.value;
+    if(selectedValue === "hPencil"){
+        evt.target.style.visibility = "hidden";
+        pencil3Step3.style.visibility = "visible";
+        arr2Step3.style.visibility = "visible";
+    }else{
+        alert("Please select H Pencil");
+    }
+});
+
+pencil3Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr2Step3.style.visibility = "hidden";
+    png5Step3.style.visibility = "hidden";
+
+    gif5Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        gif5Step3.style.visibility = "hidden";
+        png6Step3.style.visibility = "visible";
+
+        instruction1Step3.innerText = "Select 3H pencil and similarly join the 3rd, 4th and 5th divisions with the point B.";
+
+        trans6Step3.style.visibility = "visible";
+        arr1Step3.style.visibility = "visible";
+    }, 10000);
+});
+
+trans6Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+
+    dropMenu4Step3.style.visibility = "visible";
+});
+
+dropMenu4Step3.addEventListener("change",(evt)=>{
+    let selectedValue = dropMenu4Step3.value;
+    if(selectedValue === "3hPencil"){
+        evt.target.style.visibility = "hidden";
+        pencil4Step3.style.visibility = "visible";
+        arr2Step3.style.visibility = "visible";
+    }else{
+        alert("Please select 3H Pencil");
+    }
+});
+
+pencil4Step3.addEventListener("click", (evt)=> {
+    evt.target.style.visibility = "hidden";
+    arr2Step3.style.visibility = "hidden";
+    png6Step3.style.visibility = "hidden";
+
+    gif6Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        trans7Step3.style.visibility = "visible";
+        arr2Step3.style.visibility =  "visible";
+    }, 10000);
+});
+
+trans7Step3.addEventListener("click", (evt) =>{
+    evt.target.style.visibility = "hidden";
+    arr2Step3.style.visibility = "hidden";
+    gif6Step3.style.visibility = "hidden";
+
+    gif7Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        trans8Step3.style.visibility = "visible";
+        arr2Step3.style.visibility =  "visible";
+    }, 10000);
+});
+
+trans8Step3.addEventListener("click", (evt) =>{
+    evt.target.style.visibility = "hidden";
+    arr2Step3.style.visibility = "hidden";
+    gif7Step3.style.visibility = "hidden";
+
+    gif8Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        gif8Step3.style.visibility = "hidden";
+        png7Step3.style.visibility =  "visible";
+
+        instruction1Step3.innerText = "Select the compass, with AB as the radius  and starting from the 2nd point, mark off arcs on the line B-3, B-4 and B-5.";
+
+        trans9Step3.style.visibility = "visible";
+        arr3Step3.style.visibility = "visible";
+    }, 10000);
+});
+
+trans9Step3.addEventListener("click", (evt)=>{
+    evt.target.style.visibility = "hidden";
+    arr3Step3.style.visibility = "hidden";
+
+    comp2Step3.style.visibility = "visible";
+    arr2Step3.style.visibility = "visible";
+});
+
+comp2Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr2Step3.style.visibility = "hidden";
+    png7Step3.style.visibility = "hidden";
+
+    gif9Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        gif9Step3.style.visibility = "hidden";
+        png8Step3.style.visibility = "visible";
+
+        instruction1Step3.innerText = "Now, select H pencil and join all the arcs to obtain the required hexagon.";
+
+        trans10Step3.style.visibility = "visible";
+        arr1Step3.style.visibility = "visible";
+    }, 15000);
+});
+
+trans10Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+
+    dropMenu5Step3.style.visibility = "visible";
+});
+
+dropMenu5Step3.addEventListener("change",(evt)=>{
+    let selectedValue = dropMenu5Step3.value;
+    if(selectedValue === "hPencil"){
+        evt.target.style.visibility = "hidden";
+        pencil5Step3.style.visibility = "visible";
+        arr2Step3.style.visibility = "visible";
+    }else{
+        alert("Please select H Pencil");
+    }
+});
+
+pencil5Step3.addEventListener("click", (evt)=> {
+    evt.target.style.visibility = "hidden";
+    arr2Step3.style.visibility = "hidden";
+    png8Step3.style.visibility = "hidden";
+
+    gif10Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        trans11Step3.style.visibility = "visible";
+        arr2Step3.style.visibility =  "visible";
+    }, 9000);
+});
+
+trans11Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr2Step3.style.visibility = "hidden";
+    gif10Step3.style.visibility = "hidden";
+
+    gif11Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        trans12Step3.style.visibility = "visible";
+        arr6Step3.style.visibility =  "visible";
+    }, 9000);
+});
+
+trans12Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr6Step3.style.visibility = "hidden";
+    gif11Step3.style.visibility = "hidden";
+
+    gif12Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        trans13Step3.style.visibility = "visible";
+        arr6Step3.style.visibility =  "visible";
+    }, 9000);
+});
+
+trans13Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr6Step3.style.visibility = "hidden";
+    gif12Step3.style.visibility = "hidden";
+
+    gif13Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        nextToFinalPng.style.visibility = "visible";
+        arrNext.style.visibility =  "visible";
+    }, 9000);
+});
+
+nextToFinalPng.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arrNext.style.visibility = "hidden";
+    gif13Step3.style.visibility = "hidden";
+    assemblyBox.style.visibility = "hidden";
+    statementBox.style.visibility = "hidden";
+
+    finalPng.style.visibility = "visible";
+    finalPng.style.top = "80px";
+
+    setTimeout(() => {
+        nextBtnExp3.style.visibility = "visible";
+        arrNext.style.visibility = "visible";
+    }, 3000);
+});
+
+nextBtnExp3.addEventListener("click", (evt) => {
+    parent.location = `../exp-list.html`;
+});
---
+++ simulation/js/starter.js
@@ -0,0 +1,128 @@
+// VARIABLES
+const mainPage = document.querySelector("#mainPage");
+const setup1Page = document.querySelector("#setupPage1");
+const setup2Page = document.querySelector("#setupPage2");
+
+// IMAGES
+const png1Starter = document.querySelector(".starter-png1");
+const png1Sheet = document.querySelector("#png1-sheet");
+
+// 3D CLIPS 
+const clip1_3d = document.querySelector("#clip1-3d-starter");
+const clip2_3d = document.querySelector("#clip2-3d-starter");
+const clip3_3d = document.querySelector("#clip3-3d-starter");
+const clip4_3d = document.querySelector("#clip4-3d-starter");
+
+// NORMAL CLIPS
+const clip1Normal = document.querySelector("#clip1-starter");
+const clip2Normal = document.querySelector("#clip2-starter");
+const clip3Normal = document.querySelector("#clip3-starter");
+const clip4Normal = document.querySelector("#clip4-starter");
+
+// DRAFTERS
+const drafter1Starter = document.querySelector("#drafter1-starter");
+const drafter2Starter = document.querySelector("#drafter2-starter");
+
+// ARROWS
+const arrSetup1 = document.querySelector(".arr-setup1");
+const arrSetup2 = document.querySelector("#arr1-setup2");
+
+//  NEXT BUTTONS
+const arrNext = document.querySelector(".arr-next");
+const nextToSetup1 = document.querySelector("#next-to-setup1");
+const nextToSetup2 = document.querySelector("#next-to-setup2");
+const nextToExps = document.querySelector("#nextBtnOfStarter");
+
+// ############################################################################
+
+addEventListener("DOMContentLoaded", (evt) => {
+    setTimeout(() => {
+        nextToSetup1.style.visibility = "visible";
+    }, 3000);
+})
+
+// SETUP 1
+nextToSetup1.addEventListener("click", () => {
+    mainPage.style.visibility = "hidden";
+    png1Starter.style.visibility = "hidden";
+    nextToSetup1.style.visibility = "hidden";
+
+    setup1Page.style.visibility = "visible";
+})
+
+png1Sheet.addEventListener("click", (evt) => {
+    arrSetup1.style.visibility = "hidden";
+    evt.target.style.animationPlayState = "running";
+    setTimeout(()=> {
+       arrNext.style.visibility = "visible";
+        nextToSetup2.style.visibility = "visible"
+    }, 1500);
+})
+
+// SETUP 2 
+nextToSetup2.addEventListener("click",(evt) => {
+    evt.target.style.visibility = "hidden";
+    arrNext.style.visibility = "hidden";
+    setup1Page.style.visibility = "hidden"
+
+    setup2Page.style.visibility = "visible";
+    setTimeout(timeDelay1Setup2, 500);
+})
+
+let clicks = 0;     // clicks will record the number of clicks in order to hide arrow and show next button for next experiment
+
+function timeDelay1Setup2(){
+    arrSetup2.style.visibility = "visible";
+    clip1_3d.style.visibility = "visible";
+    clip2_3d.style.visibility = "visible";
+    clip3_3d.style.visibility = "visible";
+    clip4_3d.style.visibility = "visible";
+    drafter1Starter.style.visibility = "visible";
+}
+
+clip1_3d.addEventListener("click", () => {
+    clip1_3d.style.visibility = "hidden";
+    clip1Normal.style.visibility = "visible";
+    clip1Normal.style.animationPlayState = "running";
+    showNextBtn(clicks += 1);
+});
+
+clip2_3d.addEventListener("click", () => {
+    clip2_3d.style.visibility = "hidden";
+    clip2Normal.style.visibility = "visible";
+    clip2Normal.style.animationPlayState = "running";
+    showNextBtn(clicks += 1);
+});
+
+clip3_3d.addEventListener("click", () => {
+    clip3_3d.style.visibility = "hidden";
+    clip3Normal.style.visibility = "visible";
+    clip3Normal.style.animationPlayState = "running";
+    showNextBtn(clicks += 1);
+});
+
+clip4_3d.addEventListener("click", () => {
+    clip4_3d.style.visibility = "hidden";
+    clip4Normal.style.visibility = "visible";
+    clip4Normal.style.animationPlayState = "running";
+    showNextBtn(clicks += 1);
+});
+
+drafter1Starter.addEventListener("click", () => {
+    drafter1Starter.style.visibility = "hidden";
+    drafter2Starter.style.visibility = "visible";
+    drafter2Starter.style.animationPlayState = "running";
+    showNextBtn(clicks += 1);
+});
+
+function showNextBtn(clicks){
+    if(clicks === 1){
+        arrSetup2.style.visibility = "hidden";
+    }
+    if(clicks === 5){
+        setTimeout(() => {
+        nextToExps.style.visibility = "visible";
+        arrNext.style.visibility = "visible";
+    }, 1000);
+    }
+}
